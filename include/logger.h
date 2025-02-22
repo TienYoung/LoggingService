3b7b778ab4684e3663cbbdbafa4c23b9c0e8436b
Tian Yang <tyang2896@conestogac.on.ca>
2025-02-22 17:38:40 -0500

    Update arguments

--- include/logger.h
+++ include/logger.h
@@ -9,17 +9,16 @@
 
 class Logger
 {
+public:
+    Logger(int port, const char *filename);
+    ~Logger();
+
+    void Process();
+    void Log(const char *message);
+
 private:
     socket_t m_server;
     int m_receiveBufferSize;
     std::vector<char> m_receiveBuffer;
     std::ofstream m_filestream;
-    
-public:
-    Logger(int port, const char* filename);
-    ~Logger();
-
-    void Process();
-    void Log(const char *message);
 };
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,36 @@
 #include "logger.h"
 
 #include <iostream>
+#include <cstring>
+#include <charconv>
+
+enum OPTION {PORT, LOG_PATH, NUM} ;
+const char* g_options[] = {"-p", "-l"};
 
 int main(int argc, char* argv[])
 {
+    int port = 50000;
+    std::string filename = "log.lnav";
+    for(int i = 1; i < argc; i++)
+    {
+        if(strcmp(argv[i], g_options[PORT]) == 0)
+        {
+            auto [ptr, ec] = std::from_chars(argv[i+1], argv[i+1] + strlen(argv[i+1]), port); // Convert str to int
+            if (ec != std::errc())
+            {
+                std::cerr << "This is not a valid port number!" << std::endl;
+                return EXIT_FAILURE;
+            }
+        }
+        else if(strcmp(argv[i], g_options[LOG_PATH]) == 0)
+        {
+            filename = argv[i+1];
+        }
+    }
+
     try
     {
-        Logger log(50000, "log.lnav");
+        Logger log(port, filename.c_str());
         while (true)
         {
             log.Process();
@@ -14,7 +38,7 @@ int main(int argc, char* argv[])
     }
     catch(const std::system_error& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
