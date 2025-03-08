6c9ad512d81018f8a3bf4ecf953e99ea9b3ad07a
Aska <constantine.sky@hotmail.com>
2025-03-08 02:01:17 -0500

    Refactor main function: streamline config loading and enhance argument handling

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,20 +4,23 @@ use toml::Table;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut port = 50000;
-    let mut filename = String::from("log.lnav");
-    let mut rate_limit = 100;
 
-    let config: Table = fs::read_to_string("config.toml")
-        .expect("ERROR: Could not read config.toml")
+    let config: Table = fs::read_to_string("./config.toml")
+        .expect(&format!("ERROR: Could not read config.toml in {}", std::env::current_dir().unwrap().display()))
         .parse::<Table>()
         .expect("ERROR: Could not parse config.toml");
 
-    port = config["Service"]["port_number"].as_integer().unwrap();
-    filename = config["Service"]["log_file"].as_str().unwrap().to_string();
-    rate_limit = config["Service"]["rate_limit"].as_integer().unwrap();
+    let port = config["Service"]["port_number"].as_integer().unwrap();
+    let filename = config["Service"]["log_file"].as_str().unwrap().to_string();
+    let rate_limit = config["Service"]["rate_limit"].as_integer().unwrap();
 
-    println!("Port: {}", port);
-    println!("Filename: {}", filename);
-    println!("Rate Limit: {}", rate_limit);
+    for arg in args.iter() {
+        match arg.as_str() {
+            "-p" => println!("Port number: {}", port),
+            "-l" => println!("Log file: {}", filename),
+            "-r" => println!("Rate limit: {}", rate_limit),
+            _ => println!("Unknown argument: {}", arg),
+            
+        }
+    }
 }
